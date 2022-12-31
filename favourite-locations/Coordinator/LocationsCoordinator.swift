31d158cce3a32ab347b481c9fcc546b63f9bec6e
Irek Khabibullin <irek.rifson@gmail.com>
2023-01-01 01:54:45 +0300

    add coordinator

--- favourite-locations/Coordinator/LocationsCoordinator.swift
+++ favourite-locations/Coordinator/LocationsCoordinator.swift
@@ -5,4 +5,25 @@
 //  Created by Ирек Хабибуллин on 30.12.2022.
 //
 
-import Foundation
+import UIKit
+
+protocol CoordinatorProtocol: AnyObject {
+    var tabBarController: UITabBarController! { get }
+}
+
+class LocationsCoordinator: NSObject, CoordinatorProtocol {
+    var tabBarController: UITabBarController!
+    let locationsPresenter: LocationsPresenterProtocol
+    let mapPresenter: MapPresenterProtocol
+    
+    init(locationsPresenter: LocationsPresenterProtocol, mapPresenter: MapPresenterProtocol, tabBarController: UITabBarController) {
+        self.locationsPresenter = locationsPresenter
+        self.mapPresenter = mapPresenter
+        self.tabBarController = tabBarController
+        super.init()
+    }
+}
+
+extension LocationsCoordinator: UITabBarControllerDelegate {
+    
+}
